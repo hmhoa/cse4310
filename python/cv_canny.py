534988d83b989e616ae0a96fd6816ba0198956d6
Hoang Ho <hoang.ho@mavs.uta.edu>
2022-10-19 14:47:48 -0500

    Add lecture notes

--- python/cv_canny.py
+++ python/cv_canny.py
@@ -12,9 +12,21 @@ while(True):
     
     # check to see if we received a valid frame
     if success:
+        # may want to do some grayscale or filters
 
         # apply the canny edge detector
+        # old but fast, parallelizable and tunable
+        # canny edge detector algo needs 2 tuning values
+        # threshold to see whats light and dark(?)
+        # <100 = dark
+        # >200 = very light
+        # how canny works - takes a sliding kernel (sample space) and move it
+        # when it finds a big difference, it marks the center of that kernel as an edge
+        # very common preprocessing step
         edges = cv2.Canny(frame, 100, 200)
+
+        # any lines detected aren't actually defined as "lines" theyre just pixels
+        # huff line detection - model lines - clusters of pixels that fit a y=mx+b type of line
 
         # Display the resulting frames
         cv2.imshow('frame', frame)
@@ -32,3 +44,9 @@ while(True):
 # shutdown the capture stream and display windows
 capture.release()
 cv2.destroyAllWindows()
+
+# can load in trained models to opencv w their dnn module and use it in opencv
+
+# cv boils down to:
+# 1st half - performance, but some apps we dont care abt performance
+# 2nd half - knowing whats in our library and the api-know how to search for them in docs
--- python/cv_capture.py
+++ python/cv_capture.py
@@ -1,26 +1,61 @@
+# 10/19/2022
+
+# 2 way to install - package, src code (can manually adjust optimizations)
+
+# opencv up to version 5.x but all in opencv2
+
+# OpenCV
+#     - Supported in C++ and python
+#     - De facto standard
+#         ○ Usually learning opencv when learning cv
+
+# Opencv python
+#     - Opencv python pip install
+
+# Github cse4310>python>cv_capture.py
+#     - Line 5 does most of the nitty gritty work for us
+#         ○ Can set brightness, etc.
+#         ○ Grab data from usb camera, 
+# - Can also support data from webstreams (rtsp) 
+
 # import dependencies
 import cv2
   
 # create the video capture object (USB capture)
+# connects to video device - can pull video, individual frame, from any usb as long as it supports os image acquuisition standard/driver level interface to talk to usb cam (linux - bf4l2)
+# pass in 0 to videocapture object - enum at that addy
+#   - integrated camera (unless disabled)
 capture = cv2.VideoCapture(0)
 
 # create the video capture object (RTSP stream)
+# video clip ppl often use to test video stuff
+# could easily be a security clip from your home and such
+# can even go to websites for traffic cameras - alot of them have rtsp streams and can use them
+# since traffic is a common cv use case, can just connect to the rtsp stream from somewhere
 #capture = cv2.VideoCapture("rtsp://wowzaec2demo.streamlock.net/vod/mp4:BigBuckBunny_115k.mp4")
 
 # begin polling loop
 while(True):
 
     # capture the video frame
+    # fill up frame buffer
+    # up to 30 ms to come in - proc time, latency to grab frame, acquisition
     success, frame = capture.read()
     
-    # check to see if we received a valid frame
+    # frame will come through as a matrix - cv mat
+
+    # check to see if we received a valid frame since we may not have any data left
     if success:
 
         # Display the resulting frame
+        # can overwrite windows by giving same window name, but if you change names it'll make unique windows
+        # tells opencv to add a particular window to render to its window drawing queue, but wont wont send to gpu to display until call cv waitkey
         cv2.imshow('frame', frame)
 
-        # check for 'q' key press
-        key = cv2.waitKey(1)
+        # check for 'q' key press - check for n number of ms
+        # in this case, wait 1 ms for key press detected
+        # ascii code for that key will be in the key variable
+        key = cv2.waitKey(1) # tells engine to draw the stuff
         print(key)
         if key == ord('q'):
             print("terminating program")
@@ -29,5 +64,7 @@ while(True):
         print("WARNING: unable to retrieve frame")
   
 # shutdown the capture stream and display windows
+# dont have to do this if program exits naturally-closes normally since destructors will call it
+# but if it crashes while in use, but os may still mark camera in use-wait a bit til os lets it go or force close it
 capture.release()
 cv2.destroyAllWindows()
